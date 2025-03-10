13c0760c855d6582050d57b3c404e82ebd814846
Diego <149812280+Diegooaraujo@users.noreply.github.com>
2025-03-10 11:47:09 -0300

    adicionando script

---
+++ script.js
@@ -0,0 +1,13 @@
+const tema = document.querySelector(".tema");
+const body = document.querySelector("body")
+
+tema.addEventListener("click", () => {
+   
+    if (body.classList.contains("claro")) {
+        body.classList.remove("claro")
+        tema.style.backgroundImage = 'url("./img/brilho-do-sol.png")';
+    } else {
+        body.classList.add("claro");
+        tema.style.backgroundImage = 'url("./img/lua-crescente.png")';
+    }
+});
