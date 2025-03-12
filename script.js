6b830dd6b294e21a93398ac438fe1c58e3d50818
Diego <149812280+Diegooaraujo@users.noreply.github.com>
2025-03-12 09:15:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tema = document.querySelector(".tema");
 const body = document.querySelector("body")
-const botao = document.querySelector(".botao-insta")
+
 
 tema.addEventListener("click", () => {
    
@@ -13,16 +13,4 @@ tema.addEventListener("click", () => {
     }
 });
 
-botao.addEventListener("mousemove", (event) => {
-    const rect = botao.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
 
-    if (mouseX < 10 || mouseX > rect.width - 10 || mouseY < 10 || mouseY > rect.height - 10) {
-        const newLeft = Math.random() * (window.innerWidth - rect.width);
-        const newTop = Math.random() * (window.innerHeight - rect.height);
-        botao.style.position = "absolute";
-        botao.style.left = `${newLeft}px`;
-        botao.style.top = `${newTop}px`;
-    }
-});
